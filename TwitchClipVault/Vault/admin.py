b48281cd64b8fac444f19ebe13b26a18862902f4
Blue Electric <be@leekcake.moe>
2020-10-23 17:47:18 +0900

    Add Admin Site
    Doesn't need for this project, only for study

--- TwitchClipVault/Vault/admin.py
+++ TwitchClipVault/Vault/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from Vault.models import Clip
+
+admin.site.register(Clip)
--- TwitchClipVault/Vault/models.py
+++ TwitchClipVault/Vault/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Clip(models.Model):
+    Title = models.CharField(max_length=400)
+    CreatedAt = models.DateTimeField('video created')
+    LengthInSecond = models.FloatField()
+    ViewCount = models.IntegerField()
+    YoutubeId = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.Title
